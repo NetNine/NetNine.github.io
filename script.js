18656b0641636f228eeb6eca98e8a01585171925
NetNine <115904820+NetNine@users.noreply.github.com>
2024-06-14 19:33:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-
-var typed = new Typed("multiple-text",{
-    Strings: ["FX Trader", "Ethical Hacker", "Web Developer", "Frontend Developer", "UI Designer", "Graphics Designer", "Video Editor", "Content Creator"], 
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-
-})
